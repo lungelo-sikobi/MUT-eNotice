da6a49913f6b3e4f1bc81e38ffea86a4c0fb71a8
Lungelo Sikobi <42305382+Sikobi@users.noreply.github.com>
2019-09-02 11:42:07 +0200

    Add project files.

---
+++ MUT eNotice/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MUT_eNotice
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MUT eNotice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MUT_eNotice.MvcApplication" Language="C#" %>
